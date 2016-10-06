a6947df0df66254cf3c2a701bbd84543af964e47
st1ffy69 <karpovntn@rambler.ru>
2016-10-06 18:42:55 +0300

    Рефакторинг путем извлечения вспомогательных методов

---
+++ addressbook-web-tests/1pgbMedCreationTest.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumTests
+{
+    [TestFixture]
+    public class MedCreationTests
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            driver = new FirefoxDriver();
+            baseURL = "https://1pgb.ru";
+            verificationErrors = new StringBuilder();
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+
+        [Test]
+        public void MedCreationTest()
+        {
+            driver.Navigate().GoToUrl(baseURL + "/");
+            driver.FindElement(By.CssSelector("a > span.btn-in")).Click();
+            //driver.FindElement(By.Name("Codeword")).Clear();
+            //driver.FindElement(By.Name("Codeword")).SendKeys("rested1");
+            System.Threading.Thread.Sleep(500);
+            driver.FindElement(By.CssSelector("div.scrollPopUp")).Click();
+            driver.FindElement(By.LinkText("Больничный и пособие")).Click();
+            System.Threading.Thread.Sleep(500);
+            driver.FindElement(By.XPath("(//a[contains(text(),'Алексеева Марина Владимировна')])[2]")).Click();
+            System.Threading.Thread.Sleep(15000);
+            driver.FindElement(By.XPath("//div[@id='date_container_']/span/span/span/span")).Click();
+            driver.FindElement(By.XPath("//div[@id='date_container_']/span/span/span[2]/div/div[3]/span[8]")).Click();
+            
+            driver.FindElement(By.Id("komandlabel1")).Click();
+            driver.FindElement(By.Id("komandlabel1")).Clear();
+            driver.FindElement(By.Id("komandlabel1")).SendKeys("111 111 111 111");
+            // ERROR: Caught exception [Error: Dom locators are not implemented yet!]
+            driver.FindElement(By.XPath("//body/div[9]")).Click();
+            //driver.FindElement(By.CssSelector("span.company-menu-dropdown__caller.ng-binding")).Click();
+           // driver.FindElement(By.LinkText("Выйти")).Click();
+        }
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+
+        private string CloseAlertAndGetItsText()
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert)
+                {
+                    alert.Accept();
+                }
+                else
+                {
+                    alert.Dismiss();
+                }
+                return alertText;
+            }
+            finally
+            {
+                acceptNextAlert = true;
+            }
+        }
+    }
+}
--- addressbook-web-tests/GroupCreationTests.cs
+++ addressbook-web-tests/GroupCreationTests.cs
@@ -7,10 +7,10 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Support.UI;
 
-namespace SeleniumTests
+namespace WebAddressbookTests
 {
     [TestFixture]
-    public class Untitled
+    public class GroupCreationTests
     {
         private IWebDriver driver;
         private StringBuilder verificationErrors;
@@ -40,24 +40,59 @@ namespace SeleniumTests
         }
 
         [Test]
-        public void TheUntitledTest()
+        public void GroupCreationTest()
+        {
+            OpenHomePage();
+            Login("admin", "secret");
+            GoToGroupsPage();
+            InitNewGroupCreation();
+            FillGroupForm("q", "qw", "wq");
+            SubmitGroupCreation();
+            ReturnToGroupsPage();
+        }
+
+        private void ReturnToGroupsPage()
+        {
+            driver.FindElement(By.LinkText("groups")).Click();
+        }
+
+        private void SubmitGroupCreation()
+        {
+            driver.FindElement(By.Name("submit")).Click();
+        }
+
+        private void FillGroupForm(string name, string header, string footer)
+        {
+            driver.FindElement(By.Name("group_name")).Clear();
+            driver.FindElement(By.Name("group_name")).SendKeys(name);
+            driver.FindElement(By.Name("group_header")).Clear();
+            driver.FindElement(By.Name("group_header")).SendKeys(header);
+            driver.FindElement(By.Name("group_footer")).Clear();
+            driver.FindElement(By.Name("group_footer")).SendKeys(footer);
+        }
+
+        private void InitNewGroupCreation()
+        {
+            driver.FindElement(By.XPath("(//input[@name='new'])[2]")).Click();
+        }
+
+        private void GoToGroupsPage()
+        {
+            driver.FindElement(By.LinkText("groups")).Click();
+        }
+
+        private void Login(string username, string password)
+        {
+            driver.FindElement(By.Name("user")).Clear();
+            driver.FindElement(By.Name("user")).SendKeys(username);
+            driver.FindElement(By.Name("pass")).Clear();
+            driver.FindElement(By.Name("pass")).SendKeys(password);
+            driver.FindElement(By.CssSelector("input[type=\"submit\"]")).Click();
+        }
+        
+        private void OpenHomePage()
         {
             driver.Navigate().GoToUrl(baseURL + "/addressbook/");
-            driver.FindElement(By.Name("user")).Clear();
-            driver.FindElement(By.Name("user")).SendKeys("admin");
-            driver.FindElement(By.Name("pass")).Clear();
-            driver.FindElement(By.Name("pass")).SendKeys("secret");
-            driver.FindElement(By.CssSelector("input[type=\"submit\"]")).Click();
-            driver.FindElement(By.LinkText("groups")).Click();
-            driver.FindElement(By.XPath("(//input[@name='new'])[2]")).Click();
-            driver.FindElement(By.Name("group_name")).Clear();
-            driver.FindElement(By.Name("group_name")).SendKeys("q");
-            driver.FindElement(By.Name("group_header")).Clear();
-            driver.FindElement(By.Name("group_header")).SendKeys("qw");
-            driver.FindElement(By.Name("group_footer")).Clear();
-            driver.FindElement(By.Name("group_footer")).SendKeys("wq");
-            driver.FindElement(By.Name("submit")).Click();
-            driver.FindElement(By.LinkText("groups")).Click();
         }
         private bool IsElementPresent(By by)
         {
